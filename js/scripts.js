5e056f48b7f4334b93689b6bbdaa5707e6e6eb04
Genevieve Latimer and Niki Garner <genevievelatimer@gmail.com>
2016-03-16 19:16:10 -0700

    receive user input and store data

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,21 @@ function Destination(location, season, landmarks) {
   this.landmarks = landmarks;
 }
 
+Destination.prototype.information = function () {
+  return this.location + " " + this.season + "" + this.landmarks;
+};
+
 // user interface logic
 $(document).ready(function() {
-  $
+  $("form#form-group").submit(funciton(event){
+    event.preventDefault();
+
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedSeason = $("input#new-season").val();
+    var inputtedLandmarks = $("input#new-landmarks").val();
+
+    var newDestination = new Destination(inputtedLocation, inputtedSeason, inputtedLandmarks);
+
+    $("ul")
+  }
 });
